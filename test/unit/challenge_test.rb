a5ded5270a9222768088be45cd00bf3d54186cc4
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-13 16:52:10 -0300

    moving tests to appropriated classes

--- test/unit/challenge_test.rb
+++ test/unit/challenge_test.rb
@@ -1,8 +1,34 @@
 require 'test_helper'
 
 class ChallengeTest < ActiveSupport::TestCase
+  setup  do
+    @challenge = Challenge.all.second
+  end
   test "should validate answers as normal strings" do
     challenge = Challenge.new :correct_answer => "test"
     assert challenge.valid_answer?("test")
   end
+
+   test "aprove good implementations" do
+     oks = [
+      "self.match /:\\)|:\\]/", 
+      "self.match %r{:\\)|:\\]}"
+     ]
+     oks.each do |ok|
+       assert @challenge.valid_answer?(ok), "Implementation #{ok.inspect} doesnt work yet"
+     end
+   end
+   test "keep error on bad code and return false to answer" do
+     raises = [
+      ": )",
+      "self-match? /:[)]]/"
+     ]
+     raises.each do |invalid_code|
+       assert_nothing_raised do
+         assert ! @challenge.valid_answer?(invalid_code)
+         assert @challenge.last_compiling_error
+         assert @challenge.last_compiling_error_trace
+       end
+     end
+   end
 end
--- test/unit/game_challenge_test.rb
+++ test/unit/game_challenge_test.rb
@@ -12,30 +12,6 @@ class GameChallengeTest < ActiveSupport::TestCase
    test "Game should have many challenges" do
      assert_equal 2, @game.game_challenges.size
    end
-   test "aprove good implementations" do
-     oks = [
-      "self.match /:\\)|:\\]/", 
-      "self.match %r{:\\)|:\\]}"
-     ]
-     oks.each do |ok|
-       assert @game.current_challenge.valid_answer?(ok), "Implementation #{ok.inspect} doesnt work yet"
-     end
-   end
-   test "keep error on bad code and return false to answer" do
-     raises = [
-      ": )",
-      "self-match? /:[)]]/"
-     ]
-     raises.each do |invalid_code|
-       assert_nothing_raised do
-         step =  @game.current_challenge
-         assert ! step.valid_answer?(invalid_code)
-         assert step.last_compiling_error
-         assert step.last_compiling_error_trace
-       end
-     end
-   end
-
    teardown do
      @game.destroy
    end
