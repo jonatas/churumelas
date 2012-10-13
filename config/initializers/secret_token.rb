c9d90fbb5ab4b0e1c3f4674febbb6bba53174caa
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-12 21:04:13 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Churumelas::Application.config.secret_token = '9a50a809cc9380ef5777f6c6f8115df81a8e429e4c098b8fecfaf27bb3bff0914a51694a815c5f9c85c33680ab717a8320d4283fbc4f9680ece5ea455aeb123b'
