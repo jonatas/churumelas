6f64d235b2917ce184da0c272ae31aa85612db39
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-13 20:33:24 -0300

    table wrong

--- db/migrate/20121013185457_add_compiling_fields_to_challenges.rb
+++ db/migrate/20121013185457_add_compiling_fields_to_challenges.rb
@@ -1,7 +1,7 @@
 class AddCompilingFieldsToChallenges < ActiveRecord::Migration
   def change
-    add_column :challenges, :last_compiling_error, :text
-    add_column :challenges, :last_compiling_error_trace, :text
-    add_column :challenges, :tries_before_sucess, :integer, :default => 0
+    add_column :game_challenges, :last_compiling_error, :text
+    add_column :game_challenges, :last_compiling_error_trace, :text
+    add_column :game_challenges, :tries_before_sucess, :integer, :default => 0
   end
 end
