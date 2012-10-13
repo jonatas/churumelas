f7b99772c6b62fc135ba79f8e7d219397b157105
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-12 23:33:31 -0300

    bad words are not allowed

--- test/functional/game_controller_test.rb
+++ test/functional/game_controller_test.rb
@@ -11,10 +11,12 @@ class GameControllerTest < ActionController::TestCase
     assert_redirected_to :action => "next_level" 
   end
   test "should not start by typing other tries" do
-    post(:answer, :console => { :code => "strat" })
-
-    assert_not_nil  flash[:message]
-    assert_redirected_to :action => "start" 
+    bad_words = %w(nil start! \ start `` eval system)
+    bad_words.each do |try|
+      post(:answer, :console => { :code => try})
+      assert_not_nil  flash[:message]
+      assert_redirected_to :action => "start" 
+    end
   end
 
   test "should get next_level" do
