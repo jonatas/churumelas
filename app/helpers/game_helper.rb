21beb9e3b693f6c5e541d5a9d4cdeffe200dce00
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-14 18:50:37 -0300

    fixed links

--- app/helpers/game_helper.rb
+++ app/helpers/game_helper.rb
@@ -1,2 +1,17 @@
 module GameHelper
+
+  def override_username 
+    {
+      "jonatasdp" => { 
+        "github" => "jonatas", 
+        "google" => "100136762810302102952"
+      },"eliegejachini" => {"google" => "105775662756374782259"}
+    }
+  end
+  def override_url
+    {
+      "instagram" => lambda {|c,u| "http://web.#{c}.com/p/#{u}"},
+      "google" => lambda {|c,u| "http://plus.#{c}.com/#{u}"}
+    }
+  end
 end
