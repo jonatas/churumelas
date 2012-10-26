3dab15b1d305de00172eeb38273a4d7e2c6d1729
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-25 23:12:34 -0200

    rvm path try 5

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,5 @@
 require "rvm/capistrano"
 require 'bundler/capistrano'
-set :bundle_flags, "--deployment --verbose"
-
-GITHUB_REPOSITORY_NAME = 'churumelas'
-LINODE_SERVER_HOSTNAME = '66.228.54.36'
-
-#############################################
-#############################################
-
-# General Options
 
 set :rvm_bin_path, "/usr/local/rvm/bin"
 set :bundle_flags,               "--deployment"
@@ -18,15 +9,13 @@ set :deploy_to,                  "/var/www/apps/churumelas"
 set :normalize_asset_timestamps, false
 set :rvm_type, :system
 set :rails_env,                  "production"
-
 set :user,                       "root"
-
 set :scm,        :git
-set :repository, "git@github.com:jonatas/#{GITHUB_REPOSITORY_NAME}.git"
+set :repository, "git@github.com:jonatas/#{application}.git"
 set :branch,     "master"
 
-role :app, LINODE_SERVER_HOSTNAME
-role :db,  LINODE_SERVER_HOSTNAME, :primary => true
+role :app, hostname
+role :db,  hostname, :primary => true
 
 namespace :deploy do
   desc "Restarting jetty_rails"
@@ -39,7 +28,7 @@ namespace :deploy do
   end
   desc "Starting rails app with jetty_rails"
   task :start  do
-    run "cd #{current_path} && #{try_runner} script/rails server -p 80 -d -e production ", :pty => true
+    run "cd #{current_path} && script/rails server -p 80 -d -e production ", :pty => true
   end
 end
 
