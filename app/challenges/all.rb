3d07ef7e7274e195abde3fd3f0d6c7750efce88f
Jônatas Paganini <jonatasdp@gmail.com>
2012-10-28 18:22:45 -0200

    reorganizing and keeping reverse order to develop

--- app/challenges/all.rb
+++ app/challenges/all.rb
@@ -1,28 +1,32 @@
 Challenges = [ ]
 def challenge opts
   challenge = Class.new
-  opts[:level] ||= Challenges.size + 1
+  opts[:level] = Challenges.size + 1
   opts.each do |opt, with_value|
     challenge.define_singleton_method opt, lambda { with_value }
   end
+  challenge.define_singleton_method :to_s, lambda { [self.level,self.title].join(" - ") }
   Challenges << challenge
 end
 Challenges.class_eval do
-  def [] level
-      find {|challenge|challenge.level == level} or 
-      raise "Nao encontrou #{level} em #{Challenges.collect(&:level).inspect }"
+  def get_level level
+    find {|challenge|challenge.level == level} or
+    raise "There's no level #{level} on #{Challenges.collect(&:level).inspect}"
   end
 end
-%w(
-  cog_job_face_builder_with_noose
-  cog_job_face_builder_starting_for_fun
+challenges = %w(
   start
   set_your_username
   cog_job_is_for_you
   cog_job_recognize_different_smiles
   cog_job_recognize_different_smiles_and_eyes
   cog_job_recognize_smiles_in_reverse_mode
-  do_you_wanna_continue).each do |challenge|
-
+  do_you_wanna_continue
+  cog_job_face_builder_with_noose
+  cog_job_face_builder_starting_for_fun
+  define_singleton_methods
+) # TODO: erase these line when it grow up to 100 challenges
+challenges.reverse! if Rails.env.development? #I'm aways developing the last challenge
+challenges.each do |challenge|
   require challenge
 end
